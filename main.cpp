6feb24131e36f5d12eb63808313725a62ae41e60
Nircek <marcinek0302@gmail.com>
2017-09-03 17:41:27 +0200

    new design of window

--- main.cpp
+++ main.cpp
@@ -1,23 +1,22 @@
-#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 #include <cstring>
 
 #define WINDOWWIDTH         800
 #define WINDOWHEIGHT        600
 #define TITLE               "Radio Range"
 #define VERSIONNAME         "v0.0"
-#define WINDOWFULLSCREEN    false
+#define WINDOWFULLSCREEN    true
 
 int main()
 {
-    sf::Window window( sf::VideoMode( WINDOWWIDTH, WINDOWHEIGHT, 32 ),  std::string(TITLE)+" "+VERSIONNAME, WINDOWFULLSCREEN?sf::Style::Fullscreen:sf::Style::Default);
+    sf::RenderWindow window(sf::VideoMode(WINDOWWIDTH,WINDOWHEIGHT,32),std::string(TITLE)+" "+VERSIONNAME,WINDOWFULLSCREEN?sf::Style::Fullscreen:sf::Style::Default);
     while(window.isOpen()){
         sf::Event newEvent;
-        while( window.pollEvent( newEvent ) )
-        {
-            if(newEvent.type==sf::Event::Closed){
-                window.close();
-            }
+        while(window.pollEvent(newEvent)){
+            if(newEvent.type==sf::Event::Closed)window.close();
+            if(newEvent.type==sf::Event::KeyPressed&&newEvent.key.code==sf::Keyboard::Escape)window.close();
         }
+        window.clear(sf::Color(128,128,128));
         window.display();
     }
     return 0;
