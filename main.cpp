77dfc7447ff4b318878ca564cdc0c625925174cb
Nircek <marcinek0302@gmail.com>
2017-09-03 17:35:41 +0200

    base window

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
+#include <SFML/Window.hpp>
+#include <cstring>
 
+#define WINDOWWIDTH     800
+#define WINDOWHEIGHT    600
+#define TITLE           "Radio Range"
+#define VERSIONNAME     "v0.0"
 
 int main()
 {
+    sf::Window oknoAplikacji( sf::VideoMode( WINDOWWIDTH, WINDOWHEIGHT, 32 ),  std::string(TITLE)+" "+VERSIONNAME);
     return 0;
 }
