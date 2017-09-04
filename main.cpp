b63bda2c0ca8e536200f01bd4d19bf7fbc07c4e3
Nircek <marcinek0302@gmail.com>
2017-09-04 16:54:52 +0200

    add mapview & generating map from seed

--- main.cpp
+++ main.cpp
@@ -1,18 +1,50 @@
 #include <SFML/Graphics.hpp>
 #include <cstring>
 
-#define WINDOWWIDTH         800
-#define WINDOWHEIGHT        600
 #define TITLE               "Radio Range"
 #define VERSIONNAME         "v0.0"
-#define WINDOWFULLSCREEN    true
 
+#define WINDOWWIDTH         800
+#define WINDOWHEIGHT        600
+#define WINDOWFULLSCREEN    false
+
+#define MAPWIDTH            600
+#define MAPHEIGHT           600
+#define MAPSEED             1234
+
+#define MAPVIEWWIDTH        600
+#define MAPVIEWHEIGHT       600
+#define MAPVIEWSIZE         MAPVIEWWIDTH*MAPVIEWHEIGHT
+#define MAPVIEWXOFFSET      100
+#define MAPVIEWYOFFSET      0
+#define MAPVIEWXSCROLL      0
+#define MAPVIEWYSCROLL      0
+
+typedef uint8_t byte;
 using namespace sf;
 int main()
 {
-    RenderWindow window(sf::VideoMode(WINDOWWIDTH,WINDOWHEIGHT,32),std::string(TITLE)+" "+VERSIONNAME,WINDOWFULLSCREEN?sf::Style::Fullscreen:sf::Style::Default);
+    RenderWindow window(sf::VideoMode(WINDOWWIDTH,WINDOWHEIGHT,32),std::string(TITLE)+" "+VERSIONNAME,WINDOWFULLSCREEN?Style::Fullscreen:Style::Default);
     //init
-
+    byte map[MAPWIDTH][MAPHEIGHT];
+    size_t x,y,i;
+    byte tmp;
+    std::srand(MAPSEED);
+    for(x=0;x<MAPWIDTH;++x)
+        for(y=0;y<MAPHEIGHT;++y)
+            map[x][y]=std::rand();
+    sf::VertexArray mapview(sf::Points, MAPVIEWSIZE);
+    for(i=0;i<MAPVIEWSIZE;++i){
+        x=i%MAPWIDTH;
+        y=i/MAPWIDTH;
+        tmp=map[x+MAPVIEWXSCROLL][y+MAPVIEWYSCROLL];
+        mapview[i].position.x=x+MAPVIEWXOFFSET;
+        mapview[i].position.y=y+MAPVIEWYOFFSET;
+        mapview[i].color.a=255;
+        mapview[i].color.r=tmp;
+        mapview[i].color.g=tmp;
+        mapview[i].color.b=tmp;
+    }
     //----
     while(window.isOpen()){
         Event newEvent;
@@ -36,6 +68,7 @@ int main()
         }
         window.clear(sf::Color(128,128,128));
         //code
+        window.draw(mapview);
 
         //----
         window.display();
