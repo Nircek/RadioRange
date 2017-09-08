16fb1d657cb323e2966b7a62e613e48ebe4ab1e5
Nircek <marcinek0302@gmail.com>
2017-09-08 22:35:23 +0200

    add libnoise library

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <SFML/Graphics.hpp>
+#include "noise/noise.h"
+#include "noiseutils.h"
 #include <cstring>
 #include <ctime>
+
+#include <iostream>
 
 #define TITLE               "Radio Range"
 #define VERSIONNAME         "v0.0"
@@ -21,34 +25,56 @@
 #define MAPVIEWXSCROLL      0
 #define MAPVIEWYSCROLL      0
 
-typedef uint8_t byte;
+typedef uint32_t pixel;
 using namespace sf;
 class heightmap{
-    byte **pixels;
+    pixel **pixels;
     size_t width, height;
     public:
     heightmap(size_t width, size_t height, unsigned int seed=std::time(NULL)){
         this->width=width;
         this->height=height;
-        pixels=new byte*[width];
+        pixels=new pixel*[width];
         for(size_t i=0;i<width;++i)
-            pixels[i]=new byte[height];
-        std::srand(seed);
+            pixels[i]=new pixel[height];
+
+
+        module::Perlin myModule;
+        myModule.SetOctaveCount (6);
+        myModule.SetFrequency (0.8);
+        myModule.SetPersistence (0.3);
+
+        utils::NoiseMap heightMap;
+
+
+
+        utils::NoiseMapBuilderPlane heightMapBuilder;
+        heightMapBuilder.SetSourceModule (myModule);
+        heightMapBuilder.SetDestNoiseMap (heightMap);
+        heightMapBuilder.SetDestSize (MAPWIDTH, MAPHEIGHT);
+        heightMapBuilder.SetBounds (2.0, 6.0, 1.0, 5.0);
+        heightMapBuilder.Build ();
+
         for(size_t x=0;x<MAPWIDTH;++x)
-            for(size_t y=0;y<MAPHEIGHT;++y)
-                pixels[x][y]=std::rand();
+            for(size_t y=0;y<MAPHEIGHT;++y){
+                double v=0.905*heightMap.GetValue(x,y)+69;
+                pixels[x][y]=v*(((pixel)1)<<(sizeof(pixel)*8-1));
+            }
+
+
     }
+
     VertexArray genmapview(size_t viewwidth, size_t viewheight,
                            size_t viewxscroll, size_t viewyscroll,
                            size_t viewxoffset, size_t viewyoffset){
         size_t x,y,size=viewwidth*viewheight;
-        byte tmp;
+        pixel tmp;
 
         VertexArray mapview(Points, size);
         for(size_t i=0;i<size;++i){
             x=i%viewwidth;
             y=i/viewwidth;
-            tmp=pixels[x+viewxscroll][y+viewyscroll];
+            tmp=pixels[x+viewxscroll][y+viewyscroll]>>(sizeof(pixel)*8-8);
             mapview[i].position.x=x+viewxoffset;
             mapview[i].position.y=y+viewyoffset;
             mapview[i].color.a=255;
@@ -64,13 +90,14 @@ class heightmap{
         delete pixels;
     }
 };
-
+using namespace noise;
 int main()
 {
     RenderWindow window(sf::VideoMode(WINDOWWIDTH,WINDOWHEIGHT,32),std::string(TITLE)+" "+VERSIONNAME,WINDOWFULLSCREEN?Style::Fullscreen:Style::Default);
     //init
     heightmap *m = new heightmap(MAPWIDTH,MAPHEIGHT,MAPSEED);
     VertexArray mapview = m->genmapview(MAPVIEWWIDTH, MAPVIEWHEIGHT, MAPVIEWXSCROLL, MAPVIEWYSCROLL, MAPVIEWXOFFSET,MAPVIEWYOFFSET);
+
     //----
     while(window.isOpen()){
         Event newEvent;
